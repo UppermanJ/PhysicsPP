9cd702dfada07cfbbff9e997417f0a6fa9f7a8b8
UppermanJ <76638846+UppermanJ@users.noreply.github.com>
2022-09-21 11:51:53 -0400

    Second Commit

---
+++ Include/PhysicsPP.hpp
@@ -0,0 +1,18 @@
+#ifndef PHYSICSPPP_HPP
+#define PHYSICSPP_HPP
+
+template <typename T>
+class PhysicsPP
+{
+private:
+    T /* data */
+
+public:
+    PhysicsPP(T /* args */);
+
+    void Menu();
+};
+
+
+
+#endif
---
+++ Sources/PhysicsPP.cpp
@@ -0,0 +1,14 @@
+#include "PhysicsPP.hpp"
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+
+template <typename T>
+PhysicsPP<T>::PhysicsPP(T)
+{
+}
+
+template <typename T>
+void PhysicsPP<T>::Menu()
+{
+};
